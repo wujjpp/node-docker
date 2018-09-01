9ad930f91955b56b0573abf645833ae6014b137c
Wu Jian Ping <wujjpp@hotmail.com>
2018-09-02 01:47:22 +0800

    cleanup

--- src/index.js
+++ src/index.js
@@ -2,15 +2,5 @@
  * Created by Wu Jian Ping on - 2018/08/25.
  */
 
-// import node from './libs/node'
-// import service from './libs/service'
-// import system from './libs/system'
-//
-// export default {
-//   node,
-//   service,
-//   system
-// }
-
 import Docker from './libs/Docker'
 export default Docker
