f024dd62d6c3528a0499644fafab448d0534548f
Wu Jian Ping <wujjpp@hotmail.com>
2018-09-01 13:49:27 +0800

    .

--- src/libs/Docker.js
+++ src/libs/Docker.js
@@ -1,3 +1,7 @@
+/**
+ * Created by Wu Jian Ping on - 2018/09/01.
+ */
+
 import Container from './Container'
 import Image from './Image'
 import Node from './Node'
@@ -10,7 +14,6 @@ export default class Docker {
   constructor(endpoint) {
     this.endpoint = endpoint
     this.request = createRequest(this.endpoint)
-
     this.container = new Container(this.request)
     this.image = new Image(this.request)
     this.node = new Node(this.request)
