a5bc0c1f67b5c2696470f765ddf6d0e9edfbe370
Wu Jian Ping <wujjpp@hotmail.com>
2018-09-06 19:36:35 +0800

    Update Image.js

--- src/libs/Image.js
+++ src/libs/Image.js
@@ -5,12 +5,20 @@
 import Compoment from './Compoment'
 
 export default class Image extends Compoment {
-  create(image, tag = 'latest') {
-    return this.request.post('/images/create', {}, {
+  create(image, tag = 'latest', credentials = null) {
+    let config = {
       params: {
         fromImage: image,
         tag
       }
-    })
+    }
+
+    if (credentials) {
+      config.headers = {
+        'X-Registry-Auth': (Buffer.from(JSON.stringify(credentials))).toString('base64')
+      }
+    }
+
+    return this.request.post('/images/create', {}, config)
   }
 }
